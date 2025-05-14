5cc963e58ad0a8758dc37a55f917c48cdb9e125a
suraj-balvanshi <suraj.balvanshi@gmail.com>
2025-05-15 01:23:25 +0530

    contact page content localized

--- src/app/(root)/contact/page.tsx
+++ src/app/(root)/contact/page.tsx
@@ -1,16 +1,18 @@
 import PerfectContactForm from "@/components/perfect/perfect-contact-form";
 import PerfectContactInfoGrid from "@/components/perfect/prefect-contact-info-grid";
-
 import Image from "next/image";
+import { useTranslations } from "next-intl";
 
 export default function ContactHero() {
+  const t = useTranslations("contact");
+
   return (
     <section className="w-full p-4 bg-white font-inter">
       <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-10 items-center">
         <div className="w-full h-64 md:h-96 relative rounded-xl overflow-hidden shadow-md">
           <Image
             src="/images/contact-us/hero.JPG"
-            alt="Contact us"
+            alt={t("heroImageAlt")}
             fill
             className="object-cover"
             priority
@@ -18,13 +20,10 @@ export default function ContactHero() {
         </div>
         <div className="text-center md:text-left">
           <h2 className="text-3xl md:text-4xl font-bold font-spaceGrotesk text-gray-900 mb-4">
-            Let’s power a greener future, together.
+            {t("heroTitle")}
           </h2>
           <p className="text-base md:text-lg text-gray-700">
-            Whether you’re a fuel distributor, industrial buyer, or partner in
-            renewable energy — we’re here to collaborate. Reach out to explore
-            biodiesel and glycerin solutions tailored for India’s growing energy
-            needs.
+            {t("heroDescription")}
           </p>
         </div>
       </div>
@@ -32,12 +31,9 @@ export default function ContactHero() {
         <div className="mx-auto max-w-2xl space-y-6">
           <div className="space-y-2 text-center">
             <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl font-spaceGrotesk">
-              Contact Us
+              {t("contactUsTitle")}
             </h1>
-            <p className="text-gray-500">
-              Fill out the form below and we'll get back to you as soon as
-              possible.
-            </p>
+            <p className="text-gray-500">{t("contactUsDescription")}</p>
           </div>
           <PerfectContactForm />
         </div>
