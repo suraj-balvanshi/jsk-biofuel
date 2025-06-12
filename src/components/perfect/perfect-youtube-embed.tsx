f28cead30da910cfab0926e9cae199e999be3b41
suraj-balvanshi <surajbalvanshi1@gmail.com>
2025-06-12 21:50:40 +0530

    dev branch - update svg, header footer color

--- src/components/perfect/perfect-youtube-embed.tsx
+++ src/components/perfect/perfect-youtube-embed.tsx
@@ -7,7 +7,10 @@ export default function PerfectYoutubeEmbed() {
   const [isLoading, setIsLoading] = useState(true);
 
   return (
-    <div className="relative w-full aspect-video rounded-xl overflow-hidden  ">
+    <div
+      className="relative mx-auto rounded-xl overflow-hidden"
+      style={{ height: "50vh", width: "calc(50vh * 9 / 16)" }}
+    >
       {isLoading && <SkeletonCard />}
       <iframe
         src="https://drive.google.com/file/d/1l73OA9QfrUoXZWkwrrAz9zKdlKX1Q2w4/preview"
