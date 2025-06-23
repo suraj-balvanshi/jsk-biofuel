4e092f73e61c776964da34d70062e13a809c2e69
suraj-balvanshi <surajbalvanshi1@gmail.com>
2025-06-23 15:40:47 +0530

    dev branch -removed linkedIn and added office address

--- src/components/perfect/perfect-footer.tsx
+++ src/components/perfect/perfect-footer.tsx
@@ -1,4 +1,10 @@
-import { FaLinkedin, FaMapLocationDot, FaWhatsapp } from "react-icons/fa6";
+import {
+  FaBuilding,
+  FaEnvelope,
+  FaMapLocationDot,
+  FaPhone,
+  FaWhatsapp,
+} from "react-icons/fa6";
 import { useTranslations } from "next-intl";
 
 export default function PerfectFooter() {
@@ -35,20 +41,22 @@ export default function PerfectFooter() {
             </li>
           </ul>
         </div>
-        <div>
+        <div className="col-span-2">
           <h4 className="text-lg font-bold mb-4 font-spaceGrotesk">
             {t("contactUs.title")}
           </h4>
-          <ul className="space-y-2">
-            <li>
+          <ul className="grid grid-cols-1 xsm:grid-cols-2 sm:grid-cols-3 gap-4">
+            <li className="xsm:col-span-2 sm:col-span-1">
               <a
                 href="mailto:info@jskbiofuel.com"
                 className="hover:text-gray-300"
               >
+                <FaEnvelope className="h-6 w-6 " />
                 {t("contactUs.email")}
               </a>
             </li>
             <li>
+              <FaPhone className="h-6 w-6" />
               <a href="tel:+919839571751" className="hover:text-gray-300">
                 {t("contactUs.phone1")}
               </a>
@@ -57,32 +65,44 @@ export default function PerfectFooter() {
                 {t("contactUs.phone2")}
               </a>
             </li>
+            <li>
+              <div className="flex flex-col">
+                <div>
+                  <FaWhatsapp className="h-6 w-6 mb-2" />
+                  <a
+                    href={`https://wa.me/${t("contactUs.whatsapp")}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="hover:text-pink-500"
+                  >
+                    <p>{t("contactUs.whatsapp")}</p>
+                  </a>
+                </div>
+                <div className="hover:text-pink-500">
+                  <a
+                    href={`https://wa.me/${t("contactUs.whatsapp2")}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="hover:text-pink-500"
+                  >
+                    <p>{t("contactUs.whatsapp2")}</p>
+                  </a>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className="hover:text-blue-400">
+                <FaMapLocationDot className="h-6 w-6" />
+                <p>{t("contactUs.address")}</p>
+              </div>
+            </li>
+            <li>
+              <div className="hover:text-blue-400">
+                <FaBuilding className="h-6 w-6" />
+                <p>{t("contactUs.office")}</p>
+              </div>
+            </li>
           </ul>
-        </div>
-        <div>
-          <h4 className="text-lg font-bold mb-4 font-spaceGrotesk">
-            {t("followUs.title")}
-          </h4>
-          <div className="flex flex-wrap gap-4">
-            <div className="hover:text-blue-400">
-              <FaLinkedin className="h-6 w-6" />
-              <p>{t("followUs.linkedin")}</p>
-            </div>
-            <div className="hover:text-pink-500">
-              <a
-                href={`https://wa.me/${t("followUs.whatsapp")}`}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <FaWhatsapp className="h-6 w-6" />
-                <p>{t("followUs.whatsapp")}</p>
-              </a>
-            </div>
-            <div className="hover:text-blue-400 flex-basis-full">
-              <FaMapLocationDot className="h-6 w-6" />
-              <p>{t("followUs.address")}</p>
-            </div>
-          </div>
         </div>
       </div>
       <div className="text-center mt-8">
