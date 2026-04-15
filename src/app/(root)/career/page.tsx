317214483bc80fb24f26d51afe27287931c0a364
suraj-balvanshi <surajbalvanshi1@gmail.com>
2026-04-15 22:20:07 +0530

    dev-brach - git action removed and form removed
    - git action is removed as auto deployed on master on vercel
    - forms removed as email need to updated

--- src/app/(root)/career/page.tsx
+++ src/app/(root)/career/page.tsx
@@ -1,14 +1,19 @@
 import PerfectCareer from "@/components/perfect/perfect-career";
 import PerfectContactCta from "@/components/perfect/perfect-contact-cta";
 import { SCROLL_FADE_IN } from "@/lib/className";
+import { useTranslations } from "next-intl";
 
 export default function Career() {
+  const m = useTranslations("miscellaneous");
   return (
     <>
       <div className={`${SCROLL_FADE_IN}`}>
-        <PerfectCareer />
+        {/* <PerfectCareer /> */}
+        <div className="flex mx-auto max-w-2xl text-3xl font-bold tracking-tighter sm:text-4xl font-spaceGrotesk text-gray-500">
+          {m("willBeBack")}
+        </div>
       </div>
-      <div className={`${SCROLL_FADE_IN}`}>
+      <div>
         <PerfectContactCta />
       </div>
     </>
